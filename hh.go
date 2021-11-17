17aca48f2648b11aef4c48e78bcb04182f0f27ca
hello <accounts_6194b40c05cc3e002d2ffd05@mail.teambition.com>
2021-11-18 05:42:07 +0800

    更新 hh.go

--- hh.go
+++ hh.go
@@ -5,6 +5,10 @@ import (
 
 func main() {
     // http.Handle("/", http.FileServer(http.Dir("C:\\Users\\devgeek\\Desktop\\pic")))
+
+
+
+    
     http.Handle("/", http.FileServer(http.Dir(".")))
     http.ListenAndServe(":8000", nil)
 }
